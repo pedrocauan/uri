1eebac96c396e0bce9838f8fd85963d104565058
pedrocauan <mainyasuo92@gmail.com>
2024-03-15 12:34:11 -0300

    Ex 35 teste de seleção

---
+++ TesteDeSelecao/src/com/exercicios/testeselecao/Main.java
@@ -0,0 +1,32 @@
+/*
+ *  
+ * Leia 4 valores inteiros A, B, C e D.  X
+ * A seguir, se B for maior do que C e se D for maior do que A,
+ * e a soma de C com D for maior que a soma de A e B
+ * e se C e D, ambos forem positivos 
+ * e se a variável A for par
+ *  escrever a mensagem "Valores aceitos", senão escrever "Valores nao aceitos". 
+ */
+package com.exercicios.testeselecao;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		int c = scanner.nextInt();
+		int d = scanner.nextInt();
+
+		if(b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0) {
+			System.out.println("Valores aceitos");
+		}
+		else {
+			System.out.println("Valores nao aceitos");
+		}
+		scanner.close();
+	}
+	
+}
+
